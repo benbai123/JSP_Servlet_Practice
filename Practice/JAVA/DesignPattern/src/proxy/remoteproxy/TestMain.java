9c3f4fea53b90609c9d40c0e50ab45488a7b4fb9
Ben <benbai123@gmail.com>
2014-05-21 13:19:46 +0800

    add Remote Proxy Pattern

---
+++ Practice/JAVA/DesignPattern/src/proxy/remoteproxy/TestMain.java
@@ -0,0 +1,40 @@
+package proxy.remoteproxy;
+
+import java.awt.AWTException;
+import java.io.UnsupportedEncodingException;
+
+import com.google.gson.JsonSyntaxException;
+
+import proxy.remoteproxy.geo.GeoServiceProxy;
+
+/** Test for Remote Proxy Pattern
+ * 
+ * Remote Proxy Pattern
+ * 		A remote proxy can be thought about the stub in the RPC call. The
+ * 		remote proxy provides a local representation of the object which is present in
+ * 		the different address location. Another example can be providing interface for
+ * 		remote resources such as web service or REST resources.
+ * 
+ * @author benbai123
+ *
+ */
+public class TestMain {
+	public static void main (String args[]) throws AWTException, InterruptedException, JsonSyntaxException, UnsupportedEncodingException {
+		String address = "アメリカ合衆国 〒10005 ニューヨーク ウォール街 59-63";
+		double[] latLng = {40.7059659, -74.0087841};
+		// get Address by Lat/Lng
+		String resultAddress = GeoServiceProxy.getAddressByLatLng(latLng);
+		// get Lat/Lng by Address
+		double[] resultLatlng = GeoServiceProxy.getLatLngByAddress(address);
+		if ("Service not avaiable!".equals(resultAddress)) {
+			// error
+		} else {
+			System.out.println(resultAddress);
+		}
+		if (resultLatlng[0] == -999) {
+			// error
+		} else {
+			System.out.println("Lat: " + resultLatlng[0] + ", Lng: " + resultLatlng[1]);
+		}
+	}
+}
---
+++ Practice/JAVA/DesignPattern/src/proxy/remoteproxy/geo/GeoServiceProxy.java
@@ -0,0 +1,88 @@
+package proxy.remoteproxy.geo;
+
+import java.io.InputStreamReader;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/** Interface for Google Geo Services
+ * 
+ * @author benbai123
+ *
+ */
+public class GeoServiceProxy {
+	public static String getAddressByLatLng (double[] latLng) {
+		try {
+			String link = "http://maps.googleapis.com/maps/api/geocode/json?latlng="
+					+ latLng[0]+","+latLng[1]
+					+"&sensor=true&language=ja";
+			String result = getResponse(link).toString();
+			JsonParser parser = new JsonParser();
+			JsonObject o = parser.parse(result).getAsJsonObject();
+	
+			return o.getAsJsonArray("results")
+						.get(0).getAsJsonObject()
+						.get("formatted_address").getAsString();
+		} catch (Exception e) {
+			return "Service not avaiable!";
+		}
+	}
+	public static double[] getLatLngByAddress (String address) {
+		try {
+			String link = "http://maps.googleapis.com/maps/api/geocode/json?address="
+					+java.net.URLEncoder.encode("59 ウォール街 マンハッタン ニューヨーク 10005 アメリカ合衆国", "UTF-8")+"&sensor=true&language=en";
+			String result = getResponse(link).toString();
+			JsonParser parser = new JsonParser();
+			JsonObject o = parser.parse(result).getAsJsonObject();
+	
+			JsonObject location = o.getAsJsonArray("results")
+				.get(0).getAsJsonObject()
+				.get("geometry").getAsJsonObject()
+				.get("location").getAsJsonObject();
+			double[] latLng = {location.get("lat").getAsDouble(),
+								location.get("lng").getAsDouble()
+							};
+			return latLng;
+		} catch (Exception e) {
+			double[] err = {-999, -999};
+			return err;
+		}
+	}
+	private static StringBuilder getResponse(String path){
+		try {
+			java.net.URL url = new java.net.URL(path);
+			java.net.HttpURLConnection uc = (java.net.HttpURLConnection) url.openConnection();
+			uc.setRequestProperty("User-agent", "Mozilla/5.0");
+
+			uc.setRequestProperty("Accept-Charset", "UTF-8"); // encoding
+			uc.setReadTimeout(30000);// timeout limit
+			uc.connect();// connect
+			int status = uc.getResponseCode();
+
+			switch (status) {
+				case java.net.HttpURLConnection.HTTP_GATEWAY_TIMEOUT://504 timeout
+					break;
+				case java.net.HttpURLConnection.HTTP_FORBIDDEN://403 forbidden
+					break;
+				case java.net.HttpURLConnection.HTTP_INTERNAL_ERROR://500 server error
+					break;
+				case java.net.HttpURLConnection.HTTP_NOT_FOUND://404 not exist
+					break;
+				case java.net.HttpURLConnection.HTTP_OK: // ok
+					InputStreamReader reader = new InputStreamReader(uc.getInputStream(), "UTF-8");
+
+					int ch;
+					StringBuilder sb = new StringBuilder("");
+					while((ch = reader.read())!= -1){
+						sb.append((char)ch);
+					}
+					return sb;
+			}
+
+		} catch (java.net.MalformedURLException e) { // invalid address format
+			e.printStackTrace();
+		} catch (java.io.IOException e) { // connection broken
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
