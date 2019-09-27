b646a79329af628ce50898ae0af2a841666d1ed4
benbai123 <benbai123@gmail.com>
2019-09-27 15:20:52 +0800

    Add 90% time
    Add 90% time

--- Practice/JAVA/Crypto/src/crypto/test/helper/PerformanceTestHelper.java
+++ Practice/JAVA/Crypto/src/crypto/test/helper/PerformanceTestHelper.java
@@ -1,5 +1,8 @@
 package crypto.test.helper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Helper class for simplify performance testing.
  * 
@@ -17,12 +20,15 @@ public abstract class PerformanceTestHelper {
 	private long _fastest;
 	/** total time used */
 	private long _total = 0;
+	private long _over90PercentFasterThan;
 	/** average time */
 	private double _average;
 	/** whether doTest is called with positive _runs */
 	private boolean _tested = false;
 	/** whether should do warm up run */
 	private boolean _shuldWarmUp = false;
+	/** */
+	private List<Long> _execTimes;
 	// Constructor
 	public PerformanceTestHelper (String name) {
 		_name = name;
@@ -54,19 +60,40 @@ public abstract class PerformanceTestHelper {
 		if (_shuldWarmUp) {
 			warmup();
 		}
-		
+		_execTimes = new ArrayList<Long>(_runs);
 		long start = System.nanoTime();
 		for (int i = 0; i < _runs; i++) {
 			run();
 			long end = System.nanoTime();
 			long used = end-start;
+			_execTimes.add(used);
 			if (i == 0 || used > _slowest) _slowest = used;
 			if (i == 0 || used < _fastest) _fastest = used;
 			_total = _total+(used);
 			start = end;
 		}
 		_average = Double.valueOf(_total)/Double.valueOf(_runs);
+		evalOver90PercentFasterThan();
 		return this;
+	}
+	private void evalOver90PercentFasterThan () {
+		if (_execTimes.size() <= 10) {
+			_over90PercentFasterThan = _slowest;
+			return;
+		}
+		List<Long> slowest10Percent = new ArrayList<Long>();
+		for (int i = 0; i < _execTimes.size(); i++) {
+			long time = _execTimes.get(i);
+			if (i < _execTimes.size()/10) {
+				slowest10Percent.add(time);
+				slowest10Percent.sort(null);
+			} else if (time > slowest10Percent.get(0)) {
+				slowest10Percent.remove(0);
+				slowest10Percent.add(time);
+				slowest10Percent.sort(null);
+			}
+		}
+		_over90PercentFasterThan = slowest10Percent.get(0);
 	}
 	/**
 	 * Output test result
@@ -78,7 +105,8 @@ public abstract class PerformanceTestHelper {
 				+"\n\t Time used: "+nanoToMicro(_total)
 				+"\n\t Fastest: "+nanoToMicro(_fastest)
 				+"\n\t Slowest: "+nanoToMicro(_slowest)
-				+"\n\t Average: "+nanoToMicro(_average) );
+				+"\n\t Average: "+nanoToMicro(_average)
+				+"\n\t Over 90% faster than: "+nanoToMicro(_over90PercentFasterThan) );
 	}
 	private static String nanoToMicro (long nano) {
 		return nanoToMicro(Double.valueOf(nano));
