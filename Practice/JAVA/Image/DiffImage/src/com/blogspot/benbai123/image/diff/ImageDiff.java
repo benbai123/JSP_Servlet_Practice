806ba7620e166175de50321982496755a22c8d12
benbai123 <benbai123@gmail.com>
2019-11-07 20:54:18 +0800

    Continue Diff Image
    Continue Diff Image

--- Practice/JAVA/Image/DiffImage/src/com/blogspot/benbai123/image/diff/ImageDiff.java
+++ Practice/JAVA/Image/DiffImage/src/com/blogspot/benbai123/image/diff/ImageDiff.java
@@ -5,25 +5,30 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.WritableRaster;
 import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import javax.imageio.ImageIO;
 
 import com.blogspot.benbai123.image.Pixel;
 
 /**
+ * Diff 2 images and highlight differences by rectangle
+ * 
+ * TODO move implementation to impl class
+ * 
  * References
  * 
  * How do you clone a BufferedImage
  * https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
+ * 
+ * calculating distance between a point and a rectangular box
+ * https://stackoverflow.com/questions/5254838/calculating-distance-between-a-point-and-a-rectangular-box-nearest-point
  * 
  * Filling a Multidimensional Array using a Stream
  * https://stackoverflow.com/questions/26050530/filling-a-multidimensional-array-using-a-stream/26979389
@@ -32,6 +37,8 @@ import com.blogspot.benbai123.image.Pixel;
  *
  */
 public class ImageDiff {
+	/** distance limitation of join points to area */
+	private static int _maxDistance = 60;
 	public static BufferedImage diffExpectedWithActual(BufferedImage expected, BufferedImage actual) throws Exception {
 		BufferedImage resultImage = getInitialResult(expected, actual);
 		List<Rectangle> differentAreas = findDifferentAreas(expected, actual);
@@ -60,14 +67,12 @@ public class ImageDiff {
 	}
 
 	private static List<Rectangle> findDifferentAreas(BufferedImage expected, BufferedImage actual) {
-		// TODO Auto-generated method stub
 		List<Point> differentPoints = findDifferentPoints(expected, actual);
 		List<Rectangle> differentAreas = buildDifferentAreas(differentPoints);
-		return null;
+		return differentAreas;
 	}
 
 	private static List<Point> findDifferentPoints(BufferedImage expected, BufferedImage actual) {
-		// TODO Auto-generated method stub
 		int width = Math.max(expected.getWidth(), actual.getWidth());
 		int height = Math.max(expected.getHeight(), actual.getHeight());
 		Pixel[][] expectedPixels = Pixel.loadPixels(expected);
@@ -87,16 +92,61 @@ public class ImageDiff {
 		return allDifferentPoints;
 	}
 
-	private static List<Rectangle> buildDifferentAreas(List<Point> differentPoints) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	private static boolean isDifferent(int x, int y, Pixel[][] expectedPixels, Pixel[][] actualPixels) {
 		if (x >= expectedPixels.length || x >= actualPixels.length) return true;
 		if (y >= expectedPixels[0].length || y >= actualPixels[0].length) return true;
 		if (!expectedPixels[x][y].equals(actualPixels[x][y])) return true;
 		return false;
+	}
+
+	private static List<Rectangle> buildDifferentAreas(List<Point> differentPoints) {
+		final List<Rectangle> differentAreas = new ArrayList<Rectangle>();
+
+		differentPoints.stream().forEach((p) -> {
+			Optional<Rectangle> closestArea = findClosestAreaOfPoint(p, differentAreas);
+			if (!closestArea.isPresent()) {
+				Rectangle rect = new Rectangle(p.x, p.y, 1, 1);
+				differentAreas.add(rect);
+			} else {
+				involvePointToArea(p, closestArea.get());
+			}
+		});
+		return differentAreas;
+	}
+
+	private static Optional<Rectangle> findClosestAreaOfPoint(Point p, List<Rectangle> differentAreas) {
+		return differentAreas.parallelStream().min(Comparator.comparing((rect) -> {
+			return getDistanceFromPointToRect(p, rect);
+		})).filter((rect) -> {
+			return isNotTooFar(p, rect);
+		});
+	}
+
+	private static boolean isNotTooFar(Point p, Rectangle rect) {
+		return getDistanceFromPointToRect(p, rect) <= _maxDistance;
+	}
+
+	private static double getDistanceFromPointToRect(Point p, Rectangle rect) {
+		int minX = new Double(rect.getMinX()).intValue();
+		int minY = new Double(rect.getMinY()).intValue();
+		int maxX = new Double(rect.getMaxX()).intValue();
+		int maxY = new Double(rect.getMaxY()).intValue();
+		int px = p.x;
+		int py = p.y;
+		int dx = IntStream.of(minX-px, 0, px-maxX)
+				.max().orElseThrow(NoSuchElementException::new);
+		int dy = IntStream.of(minY-py, 0, py-maxY)
+				.max().orElseThrow(NoSuchElementException::new);
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+
+	private static void involvePointToArea(Point p, Rectangle rect) {
+		int minX = Math.min(p.x, new Double(rect.getMinX()).intValue());
+		int minY = Math.min(p.y, new Double(rect.getMinY()).intValue());
+		int maxX = Math.max(p.x, new Double(rect.getMaxX()).intValue());
+		int maxY = Math.max(p.y, new Double(rect.getMaxY()).intValue());
+		rect.setLocation(minX, minY);
+		rect.setSize(maxX-minX, maxY-minY);
 	}
 
 	private static void markDifferentAreas(BufferedImage image, List<Rectangle> rects) {
@@ -111,19 +161,10 @@ public class ImageDiff {
 	public static void main(String[] args) throws Exception {
 		BufferedImage expected = ImageIO.read(new File("aa.png"));
 		BufferedImage actual = ImageIO.read(new File("cc.png"));
-		List<Point> differentPoints = findDifferentPoints(expected, actual);
-		for (int i = 0; i < 100; i++) {
-			List<Point> differentPoints2 = findDifferentPoints(expected, actual);
-			for (int j = 0; j < differentPoints.size(); j++) {
-				if (differentPoints.get(j).getX() != differentPoints2.get(j).getX()
-						|| differentPoints.get(j).getY() != differentPoints2.get(j).getY()) {
-					System.out.println("different");
-					break;
-				}
-			}
-		}
-		System.out.println("equal "+differentPoints.size());
-		BufferedImage result = getInitialResult(expected, actual);
+
+		BufferedImage result = diffExpectedWithActual(expected, actual);
 		ImageIO.write(result, "png", new File("output_image.png"));
+		
+		System.out.println("done");
 	}
 }
